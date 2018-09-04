a5596659e0194fe856651ec4bc0961cca8950e1f
Kovács Máté <kismate90@gmail.com>
2018-09-04 11:36:06 +0200

    Extracted contents of main method from MyEditor to AmITea main mathod.

--- src/main/java/com/codecool/am_i_tea/AmITea.java
+++ src/main/java/com/codecool/am_i_tea/AmITea.java
@@ -1,7 +1,27 @@
 package com.codecool.am_i_tea;
 
+import com.codecool.am_i_tea.text_editor.MyEditor;
+
+import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.awt.*;
+
 public class AmITea {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+    public static void main(String[] args) throws Exception {
+
+        UIManager.put("TextPane.font",
+                new Font(MyEditor.DEFAULT_FONT_FAMILY, Font.PLAIN, MyEditor.DEFAULT_FONT_SIZE));
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+
+                new MyEditor().createAndShowGUI();
+            }
+        });
     }
+
 }
