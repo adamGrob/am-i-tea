56986bc37dcbcbcdf67d013b7c6cf08a56c0a9fb
Kovács Máté <kismate90@gmail.com>
2018-09-15 18:02:06 +0200

    Created basic HTML Editor with menubar in Java FX.

--- src/main/java/com/codecool/am_i_tea/AmITea.java
+++ src/main/java/com/codecool/am_i_tea/AmITea.java
@@ -1,7 +1,46 @@
 package com.codecool.am_i_tea;
 
-public class AmITea {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.HTMLEditor;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class AmITea extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Am-I-Tea text editor");
+
+        HTMLEditor editor = new HTMLEditor();
+
+        final Menu fileMenu = new Menu("File");
+
+        final MenuItem saveFileMenuItem = new MenuItem("Save");
+        final MenuItem openFileMenuItem = new MenuItem("Open");
+        final MenuItem exitFileMenuItem = new MenuItem("Exit");
+
+        exitFileMenuItem.setOnAction(actionEvent -> Platform.exit());
+
+        fileMenu.getItems().addAll(saveFileMenuItem, openFileMenuItem, exitFileMenuItem);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().add(fileMenu);
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+
+        Scene root = new Scene(new VBox(), 640, 480);
+        ((VBox) root.getRoot()).getChildren().addAll(menuBar, editor);
+
+        primaryStage.setScene(root);
+        primaryStage.show();
     }
 }
