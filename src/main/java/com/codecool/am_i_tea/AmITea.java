9961343d666be61c93fdd12d5da69c20006b88e1
Kovács Máté <kismate90@gmail.com>
2018-09-15 20:11:48 +0200

    Added Save File to menu that creates a html file from the contents of the editor.

--- src/main/java/com/codecool/am_i_tea/AmITea.java
+++ src/main/java/com/codecool/am_i_tea/AmITea.java
@@ -8,7 +8,14 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static javafx.application.Application.launch;
 
@@ -29,6 +36,20 @@ public class AmITea extends Application {
         final MenuItem openFileMenuItem = new MenuItem("Open");
         final MenuItem exitFileMenuItem = new MenuItem("Exit");
 
+        saveFileMenuItem.setOnAction(actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("HTML files (*.html)", "*.html");
+            fileChooser.getExtensionFilters().add(extFilter);
+
+            File file = fileChooser.showSaveDialog(primaryStage);
+
+            String textToSave = editor.getHtmlText();
+
+            if(file != null){
+                saveFile(textToSave, file);
+            }
+        });
         exitFileMenuItem.setOnAction(actionEvent -> Platform.exit());
 
         fileMenu.getItems().addAll(saveFileMenuItem, openFileMenuItem, exitFileMenuItem);
@@ -42,5 +63,17 @@ public class AmITea extends Application {
 
         primaryStage.setScene(root);
         primaryStage.show();
+    }
+
+    private void saveFile(String content, File file){
+        try {
+            FileWriter fileWriter;
+
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(AmITea.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
