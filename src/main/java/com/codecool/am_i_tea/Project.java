6f0958285d7e7fae4c994872679af958e8eb5514
Kovács Máté <kismate90@gmail.com>
2018-09-16 22:02:13 +0200

    Created model class for project (directories).

---
+++ src/main/java/com/codecool/am_i_tea/Project.java
@@ -0,0 +1,20 @@
+package com.codecool.am_i_tea;
+
+public class Project {
+
+    private String name;
+    private String path;
+
+    public Project(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
