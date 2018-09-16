08d51cfa838a1297a0d45ac9ff7eaf6fd1c0546a
Kovács Máté <kismate90@gmail.com>
2018-09-16 22:02:35 +0200

    Created DAO for Files.

---
+++ src/main/java/com/codecool/am_i_tea/TextFileDAO.java
@@ -0,0 +1,14 @@
+package com.codecool.am_i_tea;
+
+public class TextFileDAO {
+
+    private TextFile currentFile;
+
+    public TextFile getCurrentFile() {
+        return currentFile;
+    }
+
+    public void setCurrentFile(TextFile currentFile) {
+        this.currentFile = currentFile;
+    }
+}
