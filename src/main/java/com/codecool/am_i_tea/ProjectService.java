96a9e62ba3cfb944fd2da35545d51a3735581674
Kovács Máté <kismate90@gmail.com>
2018-09-16 22:42:35 +0200

    Added method to project service to find all projects (directories).

--- src/main/java/com/codecool/am_i_tea/ProjectService.java
+++ src/main/java/com/codecool/am_i_tea/ProjectService.java
@@ -1,6 +1,9 @@
 package com.codecool.am_i_tea;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ProjectService {
 
@@ -13,7 +16,7 @@ public class ProjectService {
     public boolean createProject(String projectName) {
 
         String homeFolder = System.getProperty("user.home");
-        String projectPath = homeFolder + File.separator + "AmITea"+ File.separator + projectName;
+        String projectPath = homeFolder + File.separator + "AmITea" + File.separator + projectName;
 
         File project = new File(projectPath);
         if (!project.exists()) {
@@ -28,6 +31,20 @@ public class ProjectService {
         } else {
             System.out.println("Project already exists. Choose another name!");
             return false;
+        }
+    }
+
+    public List<String> getAllProjects() {
+        String homeFolder = System.getProperty("user.home");
+        String path = homeFolder + File.separator + "AmITea";
+        File file = new File(path);
+        String[] projects = file.list((current, name) -> new File(current, name).isDirectory());
+        if (projects != null) {
+            System.out.println("Found the list of all projects!");
+            return new ArrayList<>(Arrays.asList(projects));
+        } else {
+            System.out.println("Couldn't find the list of projects!");
+            return null;
         }
     }
 }
