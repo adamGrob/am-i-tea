4fdf308a4888534837ca9fe4a6243843e398742a
Kovács Máté <kismate90@gmail.com>
2018-09-17 21:06:11 +0200

    Added field pictures to text file to register how many pictures belog to the file.

--- src/main/java/com/codecool/am_i_tea/TextFile.java
+++ src/main/java/com/codecool/am_i_tea/TextFile.java
@@ -4,11 +4,23 @@ public class TextFile {
 
     private String name;
 
+    //todo read the number of pictures whenever a file is opened as well !!!!
+    private int pictures;
+
     public TextFile(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int nextPicture() {
+        pictures++;
+        return pictures;
+    }
+
+    public void nextPictureRollback() {
+        pictures --;
     }
 }
