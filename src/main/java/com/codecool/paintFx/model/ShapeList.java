6246822759dbb640558c208bea1cdf21901553fc
Kovács Máté <kismate90@gmail.com>
2018-09-19 19:27:08 +0200

    Crated Shape List as a singleton (global) class to hold all the drawn shapes of an image.

---
+++ src/main/java/com/codecool/paintFx/model/ShapeList.java
@@ -0,0 +1,25 @@
+package com.codecool.paintFx.model;
+
+import java.util.List;
+
+public class ShapeList {
+
+    private static ShapeList instance;
+
+    public static ShapeList getInstance(){
+        if (instance == null) {
+            instance = new ShapeList();
+        }
+        return instance;
+    }
+
+    private List<MyShape> shapeList;
+
+    public List<MyShape> getShapeList() {
+        return shapeList;
+    }
+
+    public void setShapeList(List<MyShape> shapeList) {
+        this.shapeList = shapeList;
+    }
+}
