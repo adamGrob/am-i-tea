e0cee250da845bb9833d3cdfdadf595f451462fe
Kovács Máté <kismate90@gmail.com>
2018-09-27 18:33:05 +0200

    Created logger service file to handle the global responsibilities of the logger.

---
+++ src/main/java/com/codecool/am_i_tea/service/LoggerService.java
@@ -0,0 +1,58 @@
+package com.codecool.am_i_tea.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerService {
+
+    private String path;
+
+    public LoggerService() {
+    }
+
+    public void initializeLogger() {
+
+        switch (System.getProperty("os.name")) {
+            case "Linux":
+                String homeFolder = System.getProperty("user.home");
+                path = homeFolder + File.separator + ".config" + File.separator + "AmITea";
+                break;
+            case "Windows":
+                String programData = System.getenv("%PROGRAMDATA%");
+                path = programData + File.separator + "AmITea" + File.separator + "config";
+                break;
+            default:
+                System.out.println("This program is only designed to work under Windows or Linux operation systems!");
+                break;
+        }
+
+        Logger amITeaLogger = Logger.getLogger("AmITeaLogger");
+        String logFilePath = path + File.separator + "logs";
+
+        LocalDateTime temp = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH-mm-ss");
+        String logFileName = "log_" + temp.toLocalDate().toString() + "T" + temp.toLocalTime().format(formatter) + ".txt";
+
+        try {
+            FileHandler logFileHandler = new FileHandler(logFilePath + File.separator + logFileName);
+            SimpleFormatter simpleFormatter = new SimpleFormatter();
+            logFileHandler.setFormatter(simpleFormatter);
+            amITeaLogger.addHandler(logFileHandler);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Logger getLogger() {
+        return Logger.getLogger("AmITeaLogger");
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
