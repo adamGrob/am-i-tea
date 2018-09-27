60b4c181aee554e930fc748d880b680e44d53f6e
Kovács Máté <kismate90@gmail.com>
2018-09-27 15:50:50 +0200

    Created PropertyUtil service class to handle project properties-related actions.

---
+++ src/main/java/com/codecool/am_i_tea/service/PropertyUtil.java
@@ -0,0 +1,12 @@
+package com.codecool.am_i_tea.service;
+
+import java.util.Properties;
+
+public class PropertyUtil {
+
+    private Properties properties;
+
+    public PropertyUtil(Properties properties) {
+        this.properties = properties;
+    }
+}
