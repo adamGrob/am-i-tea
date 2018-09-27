8355e42c73365c64aa6c327efe3d005fa907dc1e
Kovács Máté <kismate90@gmail.com>
2018-09-27 15:58:57 +0200

    Refactored initialization method to differentiate between Linux, Windows and other operation systems.

--- src/main/java/com/codecool/am_i_tea/service/PropertyUtil.java
+++ src/main/java/com/codecool/am_i_tea/service/PropertyUtil.java
@@ -13,32 +13,47 @@ public class PropertyUtil {
     }
 
     public void initializeProperties() {
-        if (System.getProperty("os.name").equals("Linux")) {
-            String homeFolder = System.getProperty("user.home");
-            String path = homeFolder + File.separator + ".config" + File.separator + "AmITea";
-            String fileName = "config.properties";
-            File configFolder = new File(path);
-            File configFile = new File(path + File.separator + fileName);
-            if (!configFolder.exists()) {
-                if (configFolder.mkdirs()) {
-                    System.out.println("Config directory created!");
-                } else {
-                    System.out.println("Failed to create config directory!");
-                }
-            }
-            if (!configFile.exists()) {
-                try {
-                    if (configFile.createNewFile()) {
-                        System.out.println("Config file created!");
-                        //todo initialiteConfigFileProperties();
-                    } else {
-                        System.out.println("Failed to create config file!");
-                    }
-                } catch (IOException ex) {
-                    System.out.println(ex.getMessage());
-                }
-            }
-            //todo readConfigProperties();
+        switch (System.getProperty("os.name")) {
+            case "Linux":
+                initializeLinux();
+                break;
+            case "Windows":
+                initializeWindows();
+                break;
+            default:
+                System.out.println("This program is only designed to work under Windows or Linux operation systems!");
+                break;
         }
+    }
+
+    private void initializeWindows() {
+    }
+
+    private void initializeLinux() {
+        String homeFolder = System.getProperty("user.home");
+        String path = homeFolder + File.separator + ".config" + File.separator + "AmITea";
+        String fileName = "config.properties";
+        File configFolder = new File(path);
+        File configFile = new File(path + File.separator + fileName);
+        if (!configFolder.exists()) {
+            if (configFolder.mkdirs()) {
+                System.out.println("Config directory created!");
+            } else {
+                System.out.println("Failed to create config directory!");
+            }
+        }
+        if (!configFile.exists()) {
+            try {
+                if (configFile.createNewFile()) {
+                    System.out.println("Config file created!");
+                    //todo initialiteConfigFileProperties();
+                } else {
+                    System.out.println("Failed to create config file!");
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        //todo readConfigProperties();
     }
 }
