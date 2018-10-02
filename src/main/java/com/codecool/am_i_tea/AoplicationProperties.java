9a36bf605da0599e047e5731bb5a8c128f1d15f3
Kovács Máté <kismate90@gmail.com>
2018-10-02 10:05:20 +0200

    Created class to hold Application Properties (basically globals).

---
+++ src/main/java/com/codecool/am_i_tea/AoplicationProperties.java
@@ -0,0 +1,17 @@
+package com.codecool.am_i_tea;
+
+public class AoplicationProperties {
+
+    private String configFolderPath;
+
+    public AoplicationProperties() {
+    }
+
+    public String getConfigFolderPath() {
+        return configFolderPath;
+    }
+
+    public void setConfigFolderPath(String configFolderPath) {
+        this.configFolderPath = configFolderPath;
+    }
+}
