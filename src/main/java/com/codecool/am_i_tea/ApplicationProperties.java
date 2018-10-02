5ca50dbabbe070831409fa9e606f431d851fe2c0
Kovács Máté <kismate90@gmail.com>
2018-10-02 10:08:38 +0200

    Added initialize method to Application Properties to determine the config folder location based on the OS.

--- src/main/java/com/codecool/am_i_tea/ApplicationProperties.java
+++ src/main/java/com/codecool/am_i_tea/ApplicationProperties.java
@@ -1,5 +1,7 @@
 package com.codecool.am_i_tea;
 
+import java.io.File;
+
 public class ApplicationProperties {
 
     private String configFolderPath;
@@ -11,7 +13,17 @@ public class ApplicationProperties {
         return configFolderPath;
     }
 
-    public void setConfigFolderPath(String configFolderPath) {
-        this.configFolderPath = configFolderPath;
+    public void initialize() {
+        String osName = System.getProperty("os.name");
+
+        if (osName.contains("Linux")) {
+            String homeFolder = System.getProperty("user.home");
+            configFolderPath = homeFolder + File.separator + ".config" + File.separator + "AmITea";
+        } else if (osName.toLowerCase().contains("windows")) {
+            String programData = System.getenv("APPDATA");
+            configFolderPath = programData + File.separator + "AmITea" + File.separator + "config";
+        } else {
+            System.out.println("This program is only designed to work under Windows or Linux operation systems!");
+        }
     }
 }
