5882c24c5853f51859e9419194d6af8d0a457fb2
Kovács Máté <kismate90@gmail.com>
2018-10-15 10:18:32 +0200

    Updated links now open new empty windows with webView and drawing canvas.

--- src/main/java/com/codecool/am_i_tea/JavaApplication.java
+++ src/main/java/com/codecool/am_i_tea/JavaApplication.java
@@ -3,7 +3,14 @@ package com.codecool.am_i_tea;
 import com.codecool.am_i_tea.dao.ProjectDAO;
 import com.codecool.am_i_tea.dao.TextFileDAO;
 import com.codecool.am_i_tea.service.TextFileService;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 public class JavaApplication {
 
@@ -21,9 +28,38 @@ public class JavaApplication {
     }
 
     public void openLinkedFile(String fileName) {
-        fileService.saveTextFile(projectDAO.getCurrentProject().getPath(),
-                fileDAO.getCurrentFile().getName(),
-                editor);
-        fileService.openTextFile(fileName, projectDAO.getCurrentProject().getPath(), editor);
+//        fileService.saveTextFile(projectDAO.getCurrentProject().getPath(),
+//                fileDAO.getCurrentFile().getName(),
+//                editor);
+//        fileService.openTextFile(fileName, projectDAO.getCurrentProject().getPath(), editor);
+
+
+        Canvas readOnlyCanvas = new Canvas();
+
+        // todo set canvas content
+
+        BorderPane readOnlyBorderPane = new BorderPane();
+        readOnlyBorderPane.getChildren().addAll(readOnlyCanvas);
+
+        WebView readOnlyEditor = new WebView();
+
+        //todo set webView content
+
+        GridPane readOnlyGridPane = new GridPane();
+        readOnlyGridPane.getChildren().addAll(readOnlyEditor);
+
+        StackPane readOnlyWrapper = new StackPane();
+        readOnlyWrapper.getChildren().addAll(readOnlyBorderPane, readOnlyGridPane);
+        Scene readOnlyWindowScene = new Scene(readOnlyWrapper, 640, 480);
+
+        Stage newWindow = new Stage();
+        newWindow.setTitle(fileName);
+        newWindow.setScene(readOnlyWindowScene);
+
+        newWindow.setX(0.0);
+        newWindow.setY(0.0);
+        newWindow.show();
+
+
     }
 }
